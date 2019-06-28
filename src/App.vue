29b093f0a4b0d53f84626346ec6786c41071d0c0
Viviana <viviana_flower@hotmail.com>
2019-06-27 23:16:21 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div>
+    <todo-list v-bind:todos="todos"></todo-list>
+    </div>
+    <div>
+      <create-todo v-on:create-todo="addTodo"></create-todo>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+
+export default {
+  name: 'app',
+components: {
+    TodoList,
+    CreateTodo,
+  },
+
+  // data function avails data to the template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+  methods: {
+    addTodo(todo) {
+    this.todos.push(todo);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
